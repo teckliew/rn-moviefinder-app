43e51ab108e973fa56baf69342e246a00332ec82
teckliew <bboytek@gmail.com>
2016-02-24 17:12:07 -0800

    init

---
+++ index.ios.js
@@ -0,0 +1,105 @@
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ListView,
+} from 'react-native';
+
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
+class movieFinder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  }
+  fetchData(){
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded: true,
+        });
+      })
+      .done();
+  }
+  componentDidMount(){
+    this.fetchData();
+  }
+  render() {
+    if(!this.state.loaded){
+      return this.renderLoadingView();
+    }
+
+    return(
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderMovie}
+        style={styles.listView}
+      />
+    );
+  }
+
+  renderLoadingView(){
+    return(
+      <View style={styles.container}>
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
+    )
+  }
+  renderMovie(movie){
+    return (
+      <View style={styles.container}>
+        <Image
+          style={styles.thumbnail}
+          source={{uri: movie.posters.thumbnail}} />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  }
+});
+
+AppRegistry.registerComponent('movieFinder', () => movieFinder);
---
+++ readme.md
@@ -0,0 +1,6 @@
+## Movie Finder React-Native App
+
+---
+
+
+Movie listview app in iOS and Android from https://facebook.github.io/react-native/docs/tutorial.html
